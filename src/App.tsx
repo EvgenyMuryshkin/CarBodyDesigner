2ea40de74abd09c14f5599184307046dc73857b1
Evgeny Muryshkin <evmuryshkin@gmail.com>
2021-09-29 21:35:23 +1000

    design management cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,7 @@ import { Icon } from './components';
 
 interface IState {
   storageModel: IStorageModel;
-
-  boxSize: IPoint3D;
-  sidePoints: IPoint2D[];
-  frontPoints: IPoint2D[];
-  topPoints: IPoint2D[];
-
+  currentDesign: IDesign | null;
   wireframes: boolean;
   flatShading: boolean;
 }
@@ -32,50 +27,47 @@ interface IStorageModel {
 export class App extends React.Component<{}, IState> {
   constructor(props: any) {
     super(props);
-    const boxSize: IPoint3D = {x: 101, y: 41, z: 31};
-//    const boxSize: IPoint3D = {x: 11, y: 7, z: 5};
-//    const boxSize: IPoint3D = {x: 3, y: 3, z: 3};
 
     this.state = {
-      boxSize,
-      sidePoints: Generate.range(0, boxSize.x).map(i => ({ x: i, y: boxSize.z })),
-      frontPoints: Generate.range(0, boxSize.y).map(i => ({ x: i, y: boxSize.z })),
-      topPoints: Generate.range(0, boxSize.x).map(i => ({ x: i, y: boxSize.y })),
+      currentDesign: null,
       wireframes: false,
       flatShading: true,
       storageModel: this.loadFromLocalStorage()
     }
   }
 
-  resetModel() {
+  newDesing(name: string): IDesign {
     const boxSize: IPoint3D = {x: 101, y: 41, z: 31};
     const sidePoints =  Generate.range(0, boxSize.x).map(i => ({ x: i, y: boxSize.z }));
     const frontPoints = Generate.range(0, boxSize.y).map(i => ({ x: i, y: boxSize.z }));
     const topPoints =  Generate.range(0, boxSize.x).map(i => ({ x: i, y: boxSize.y }));
 
-    this.setState({
+    return {
+      name,
       boxSize,
       sidePoints,
       frontPoints,
       topPoints
-    }, () => this.saveToLocalStorage());
+    }
+  }
+
+  resetModel() {
+    const { currentDesign } = this.state;
+    if (!currentDesign) return;
+
+    this.setState({
+      currentDesign: this.newDesing(currentDesign.name),
+    }, () => this.saveToLocalStorage(this.state.currentDesign));
   }
 
   componentDidMount() {
     const { storageModel } = this.state;
-    if (!storageModel?.designs?.length) {
-      this.resetModel();
-    }
-    else {
-      const design = storageModel.designs[0];
-      const { boxSize, topPoints, frontPoints, sidePoints } = design; 
-      this.setState({
-        boxSize,
-        topPoints,
-        frontPoints,
-        sidePoints
-      })
-    }
+
+    const design = storageModel?.designs[0] ?? this.newDesing("Default");
+
+    this.setState({
+      currentDesign: design
+    })
   }
 
   loadFromLocalStorage() : IStorageModel {
@@ -85,28 +77,111 @@ export class App extends React.Component<{}, IState> {
     return JSON.parse(json) as IStorageModel;
   }
 
-  saveToLocalStorage() {
-    const { boxSize, frontPoints, sidePoints, topPoints } = this.state;
+  saveToLocalStorage(design: IDesign | null) {
+    if (!design) return;
 
-    const model = this.loadFromLocalStorage();
-    model.designs = [
-      {
-        name: "Default",
-        boxSize,
-        frontPoints,
-        sidePoints,
-        topPoints
-      }
-    ];
+    const model = this.loadFromLocalStorage()
+    model.designs = model.designs.map(d => d.name === design.name ? design : d);
+    if (!model.designs.includes(design)) model.designs.push(design);
 
     const payload = JSON.stringify(model);
     localStorage.setItem("Designs", payload);
   }
 
-  render() {
-    const { storageModel, boxSize, sidePoints, frontPoints, topPoints, wireframes, flatShading } = this.state;
+  renderDesign() {
+    const { currentDesign, wireframes, flatShading } = this.state;
+    if (!currentDesign) return null;
+
+    const { boxSize, frontPoints, sidePoints, topPoints } = currentDesign;
+
     const canvasWidth = 700;
     const canvasHeight = 300;
+
+    const modifyDesign = (diff: Partial<IDesign>) => {
+      this.setState({
+        currentDesign: {
+          ...currentDesign,
+          ...diff
+        }
+      }, () => this.saveToLocalStorage(this.state.currentDesign))
+
+    }
+
+    return (
+      <table className="main-layout">
+        <tbody>
+          <tr>
+            <td>
+              <div className="side-drawing-container">
+                Front
+                <DrawingCanvas
+                  symmetrical={true}
+                  width={canvasWidth}
+                  height={canvasHeight}
+                  samples={frontPoints}
+                  maxY={boxSize.z}
+                  onChange={(newPoints) => {
+                    modifyDesign({
+                      frontPoints: newPoints
+                    })
+                  }}
+                />
+              </div>
+            </td>
+            <td>
+              <div className="side-drawing-container">
+                Top
+                <DrawingCanvas 
+                  symmetrical={false}
+                  width={canvasWidth}
+                  height={canvasHeight}
+                  samples={topPoints} 
+                  maxY={boxSize.y}
+                  onChange={(newPoints) => {
+                    modifyDesign({
+                      topPoints: newPoints
+                    })
+                  }}
+                />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="side-drawing-container">
+                Side
+                <DrawingCanvas 
+                  symmetrical={false}
+                  width={canvasWidth}
+                  height={canvasHeight}
+                  samples={sidePoints} 
+                  maxY={boxSize.z}
+                  onChange={(newPoints) => {
+                    modifyDesign({
+                      sidePoints: newPoints
+                    })
+                  }}
+                />
+              </div>
+            </td>
+            <td><AppScene 
+              bodyPoints={boxSize}
+              sidePoints={sidePoints} 
+              frontPoints={frontPoints} 
+              topPoints={topPoints}
+              wireframes={wireframes}
+              flatShading={flatShading}
+              /></td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+
+  render() {
+    const { storageModel, currentDesign, wireframes, flatShading } = this.state;
+
+    if (!currentDesign) return null;
 
     return (
       <div className="App">
@@ -121,73 +196,7 @@ export class App extends React.Component<{}, IState> {
               return <div key={d.name} className="design-selector design-selector-active">{d.name}</div>
             })}
           </div>
-          <table className="main-layout">
-            <tbody>
-              <tr>
-                <td>
-                  <div className="side-drawing-container">
-                    Front
-                    <DrawingCanvas
-                      symmetrical={true}
-                      width={canvasWidth}
-                      height={canvasHeight}
-                      samples={frontPoints}
-                      maxY={boxSize.z}
-                      onChange={(newPoints) => {
-                        this.setState({
-                          frontPoints: newPoints
-                        }, () => this.saveToLocalStorage())
-                      }}
-                    />
-                  </div>
-                </td>
-                <td>
-                  <div className="side-drawing-container">
-                    Top
-                    <DrawingCanvas 
-                      symmetrical={false}
-                      width={canvasWidth}
-                      height={canvasHeight}
-                      samples={topPoints} 
-                      maxY={boxSize.y}
-                      onChange={(newPoints) => {
-                        this.setState({
-                          topPoints: newPoints
-                        }, () => this.saveToLocalStorage())
-                      }}
-                    />
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="side-drawing-container">
-                    Side
-                    <DrawingCanvas 
-                      symmetrical={false}
-                      width={canvasWidth}
-                      height={canvasHeight}
-                      samples={sidePoints} 
-                      maxY={boxSize.z}
-                      onChange={(newPoints) => {
-                        this.setState({
-                          sidePoints: newPoints
-                        }, () => this.saveToLocalStorage())
-                      }}
-                    />
-                  </div>
-                </td>
-                <td><AppScene 
-                  bodyPoints={boxSize}
-                  sidePoints={sidePoints} 
-                  frontPoints={frontPoints} 
-                  topPoints={topPoints}
-                  wireframes={wireframes}
-                  flatShading={flatShading}
-                  /></td>
-              </tr>
-            </tbody>
-          </table>
+          {this.renderDesign()}
         </div>   
       </div>
     );
