d7be60de1b15fa3305a32be28e995618f205c86a
Evgeny Muryshkin <evmuryshkin@gmail.com>
2021-11-01 18:06:45 +1100

    youtube links, metadata fix

--- src/tutorial.tsx
+++ src/tutorial.tsx
@@ -8,21 +8,34 @@ interface IProps {
 }
 
 export class Tutorial extends React.Component<IProps> {
-    section(title: string, content: JSX.Element | null, image: string | null = null) {
+    section(title: string, content: JSX.Element | null, image: string | null = null, video: string | null = null) {
+        // style="width: 560px; height: 315px; float: none; clear: both; margin: 2px auto;"
         return (
             <div className="tutorial-section">
                 <header>{title}</header>
                 {content}
                 {image && <img alt="meaningful text" src={`tutorial/${image}.gif`} />}
+                {video && 
+                <div style={{ margin: 20 }}>
+                    <div><a style={{ textDecoration: "none" }} href={`https://youtu.be/${video}`} target="_blank">See on YouTube</a></div>
+                    <embed
+                        src={`https://www.youtube.com/embed/${video}`}
+                        type="video/mp4"
+                        width="800" height="600"
+                        title="Keyboard Cat"
+                    />
+                </div>
+                }
             </div>
         )
     }
 
-    textSection(title: string, content: string | JSX.Element | null, image: string | null = null) {
+    textSection(title: string, content: string | JSX.Element | null, image: string | null = null, video: string | null = null) {
         return this.section(
             title, 
             content ? <p>{content}</p> : null,
-            image
+            image,
+            video
             );
     }
 
@@ -33,12 +46,7 @@ export class Tutorial extends React.Component<IProps> {
             <div className="tutorial">
                 {this.section("Main Toolbar", <ToolbarLegend items={mainToolbarItems} dialogFactory={null} />)}
                 {this.section("Side Editor Toolbar", <ToolbarLegend items={sideEditorToolbar} dialogFactory={null} />)}
-                {this.textSection("Front view", "Draw front view of car body, then smooth contour", "car_front")}
-                {this.textSection("Top view", "Draw top view of car body, then smooth contour", "car_top")}
-                {this.textSection("Side view", "Draw side view of car body, then smooth contour", "car_side")}
-                {this.textSection("Add wheels", "", "car_wheels")}
-                {this.textSection("Front section", "Draw front sections view of car body, then smooth contour", "car_sections")}
-                {this.textSection("3D View", "", "car_fly_by")}
+                {this.textSection("Tutorial", "", null, "JSN41whg0-Q" )}
           </div>
         )
     }
